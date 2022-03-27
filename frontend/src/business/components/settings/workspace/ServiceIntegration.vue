1ec8fa89bbc6d1c277c22d1ff5781cbaae30d7bc
zhaoqian <qian.zhao@fit2cloud.com>
2022-03-27 14:12:19 +0800

    feature:csv版本管理

--- frontend/src/business/components/settings/workspace/ServiceIntegration.vue
+++ frontend/src/business/components/settings/workspace/ServiceIntegration.vue
@@ -4,6 +4,9 @@
       <el-tab-pane :label="$t('organization.defect_manage')" name="defect">
         <bug-management/>
       </el-tab-pane>
+      <el-tab-pane :label="$t('organization.git_repository')" name="repository" v-xpack>
+        <workspace-repository/>
+      </el-tab-pane>
     </el-tabs>
   </el-card>
 </template>
@@ -11,10 +14,12 @@
 <script>
 
   import BugManagement from "./BugManagement";
+  import WorkspaceRepository from "@/business/components/xpack/repository/WorkspaceRepository";
 
   export default {
     name: "ServiceIntegration",
     components: {
+      WorkspaceRepository,
       BugManagement
     },
     data() {
