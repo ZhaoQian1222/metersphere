6e3f880c60660bedcdfb7fad1bd58dff93348ebf
zhaoqian <qian.zhao@fit2cloud.com>
2022-04-11 21:11:16 +0800

    fix(sql): 中金环境未执行flyway后移

--- backend/src/main/resources/db/migration/V109__v1.18.1_release.sql
+++ backend/src/main/resources/db/migration/V109__v1.18.1_release.sql
@@ -1,10 +1,3 @@
-ALTER TABLE `api_definition_exec_result` ADD INDEX projectIdIndex ( `project_id` );
-ALTER TABLE `api_scenario_report` ADD INDEX projectIdIndex ( `project_id` );
-ALTER TABLE `api_scenario_report` ADD INDEX projectIdexectypeIndex ( `project_id`,`execute_type` );
--- module management
-INSERT INTO system_parameter (param_key, param_value, type, sort)
-VALUES ('metersphere.module.workstation', 'ENABLE', 'text', 1);
-
 INSERT INTO user_group_permission (id, group_id, permission_id, module_id)
 VALUES (uuid(), 'ws_admin', 'WORKSPACE_TEMPLATE:READ+API_TEMPLATE', 'WORKSPACE_TEMPLATE');
 
